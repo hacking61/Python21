8cbc5614067d4f9387246bd45b50dbb290356794
hacking61 <hacking61@hotmail.com>
2013-05-25 01:52:05 -0500

    Primera version del Hola Mundo en Python, dos diferentes manera de Escribir Hola Mundo en Python

---
+++ Python/HolaMundo.py
@@ -0,0 +1,11 @@
+'''
+Created on 25/05/2013
+
+@author: Polux
+'''
+
+#Manera 1 de Escribir Hola Mundo en Python
+print("Hola Mundo")
+
+#Manera 2 de escribir Hola Mundo en Python
+print('Hola Mundo2')
